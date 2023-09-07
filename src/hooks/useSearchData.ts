4356bafd338cbfb6ae32f459d47819db3fc3559b
teetee6 <teetee6@naver.com>
2023-09-08 02:52:24 +0900

    feat/Implement cache expiration for search results

--- src/hooks/useSearchData.ts
+++ src/hooks/useSearchData.ts
@@ -7,9 +7,10 @@ function useSearchData(
   setRecommendedResults: React.Dispatch<React.SetStateAction<SearchResult[]>>,
   setSelectedItemIndex: React.Dispatch<React.SetStateAction<number>>,
 ) {
-  const cache = useRef<{ [query: string]: SearchResult[] }>({});
+  const cache = useRef<{ [query: string]: { data: SearchResult[]; timestamp: number } }>({});
   const delayTimerRef = useRef<NodeJS.Timeout | null>(null);
   const debounceDelay = searchQuery.length > 2 ? 400 : 600;
+  const cacheExpirationTime = 20000; // 20 seconds
 
   useEffect(() => {
     if (delayTimerRef.current) {
@@ -17,23 +18,30 @@ function useSearchData(
     }
 
     if (searchQuery.trim() !== '' && cache.current[searchQuery]) {
-      setRecommendedResults(cache.current[searchQuery]);
-      setSelectedItemIndex(-1);
-    } else {
-      delayTimerRef.current = setTimeout(() => {
-        if (searchQuery.trim() !== '') {
-          getSicks(searchQuery).then(res => {
-            setRecommendedResults(res);
-            cache.current[searchQuery] = res;
-            console.info('calling api');
-            setSelectedItemIndex(-1);
-          });
-        } else {
-          setRecommendedResults([]);
-          setSelectedItemIndex(-1);
-        }
-      }, debounceDelay);
+      const cacheEntry = cache.current[searchQuery];
+      const currentTime = Date.now();
+
+      if (currentTime - cacheEntry.timestamp <= cacheExpirationTime) {
+        // Cache is still valid, use cached data
+        setRecommendedResults(cacheEntry.data);
+        setSelectedItemIndex(-1);
+        return;
+      }
     }
+
+    delayTimerRef.current = setTimeout(() => {
+      if (searchQuery.trim() !== '') {
+        getSicks(searchQuery).then(res => {
+          setRecommendedResults(res);
+          cache.current[searchQuery] = { data: res, timestamp: Date.now() }; // Update cache with data and timestamp
+          console.info('calling api');
+          setSelectedItemIndex(-1);
+        });
+      } else {
+        setRecommendedResults([]);
+        setSelectedItemIndex(-1);
+      }
+    }, debounceDelay);
 
     return () => {
       if (delayTimerRef.current) {
